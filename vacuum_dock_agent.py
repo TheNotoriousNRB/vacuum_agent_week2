d51f307abad8ac1fc8090f4367214b3e2fb35243
Niraj Rana Bhat <nirajrbrb@gmail.com>
2023-07-12 16:09:04 +1000

    Added sensor charging_dock_location_sensor to vacuum_dock_agent class

--- vacuum_dock_agent.py
+++ vacuum_dock_agent.py
@@ -6,3 +6,7 @@ class VacuumDockAgent(VacuumAgent):
     
     def __init__(self, agent_program):
         super().__init__(agent_program)
+
+    def add_all_sensors(self):
+        super().add_all_sensors()
+        self.add_sensor('charging-dock-location-sensor', (0,0), lambda v: isinstance (v, tuple) and isinstance (v[0], int) and isinstance (v[0], int))
